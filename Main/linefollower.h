11c00fadeb3ba8c7e7beae933b5375c6f52afd2f
Wietse <WietseTDam@gmail.com>
2019-03-27 12:35:50 +0100

    De calibratie funcite aangepast en een libaryt toegevoegd
    Ik heb de time.h libary toegevoegd om de tijd te meten

--- Main/linefollower.h
+++ Main/linefollower.h
@@ -11,6 +11,7 @@
 #include <signal.h>     // for catching exit signals
 #include <iomanip>		// for setw and setprecision>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -25,6 +26,7 @@ void stuur(int lijn_waarde, BrickPi3 & BP);
 bool checkVoltage(BrickPi3 & BP);
 vector<int> bepaalAfwijking(const int & hoogste, const int & laagste);
 int bepaalStuurwaarde(const int & gemiddelde, const int & laagste_verschil, const int & hoogste_verschil, const int & actuele_licht_meting);
+vector<int> calibartion(BrickPi3 & BP, sensor_color_t & Color1);
 
 
 #endif //HERSENSTORM_C1_LINEFOLLOWER_H
