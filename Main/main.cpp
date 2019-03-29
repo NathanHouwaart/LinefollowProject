e07c04f8ba63c4791618fd79a6f49fd40eb65bb6
Unknown <jochem-van-weelde@hotmail.nl>
2019-03-29 14:24:09 +0100

    Toevoeging mergen met linefollow

--- Main/main.cpp
+++ Main/main.cpp
@@ -30,31 +30,29 @@ int main() {
     sensor_color_t Color2;                                      // Initialise struct for data storage color sensor 2
     sensor_ultrasonic_t UltraSonic1;
 
-    // if(!checkVoltage(BP)) return 0;                          // Checks whether battery has enough power
+     if(!checkVoltage(BP)) return 0;                          // Checks whether battery has enough power
 
     /*-----Calibrate min and max reflection values and determine lightvalue the robot wants to follow-----*/
-    //vector<int> min_max_reflection_value = calibration(Color1, BP);
-    //vector<int> default_values = defineDifferenceToAverage((min_max_reflection_value[0]), (min_max_reflection_value[1]+100));
+    vector<int> min_max_reflection_value = calibration(Color1, BP);
+    vector<int> default_values = defineDifferenceToAverage((min_max_reflection_value[0]), (min_max_reflection_value[1]+100));
     sleep(1);
 
     /*-----Follow the line untill the ultrasonic sensor measures something withing X cm-----*/
-    /*while (true) {
+    while (true) {
         BP.get_sensor(PORT_1, Color1);                          // Read colorsensor1 and put data in struct Color1
 	    BP.get_sensor(PORT_3, Color2);
         if(getUltraSValue(PORT_4, UltraSonic1, BP) > 10){       // If the measured US distance is bigger than 10:
             if(Color2.reflected_red < (default_values[0]) && Color1.reflected_red < default_values[0]){
-                // Uncomment for debug
                 crossroad(BP);
             }else {                                             // If no intersection was detected, follow the line
                 int stuurwaarde = defineDirection(default_values[0], default_values[1], default_values[2], Color1.reflected_red);
                 stuur(stuurwaarde, BP);
             }
         }else{                                                  // If an object was detected within X cm, execute this code
-            drive(DIRECTION_STOP, 0, 360, BP);                  // Stop the car
+            drive(DIRECTION_STOP, 0, 360, BP); // Stop the car
+            driveAround(BP);        //Start driving around milk
         }
-    }*/
-
-    driveAround(BP);
+    }
 }
 
 void exit_signal_handler(int signo) {
