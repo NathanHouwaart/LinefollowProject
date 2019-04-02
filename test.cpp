f9e4f6ff7266ff081670394702796bab6999f3c4
StijnvanWijk98 <stijn.vanwijk@student.hu.nl>
2019-04-02 15:10:10 +0200

    Wietse, We kunnen nu geluid afspelen

--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main() {
     cout << "Play" << endl;
-    system("omxplayer -o local Darude\ -\ Sandstorm.mp3");
-    for (int i=0; i<1000; i++) {
-        cout << i << endl;
-    }
-    system("q");
+    system("omxplayer --no-keys -o local Sandstorm.mp3 &");
+    sleep(1);
+    system("echo 'hoi'");
+//    for (int i=0; i<100000; i++) {
+//        cout << i << endl;
+//    }
+    cout << "hooi" << endl;
+    system("fg");
+    system("2");
+    sleep(5);
+    cout << "lol" << endl;
+    system("killall omxplayer.bin");
 }
