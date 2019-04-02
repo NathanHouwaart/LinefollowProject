40cca527132d27040f480676c3458ce3c1220859
Wietse <WietseTDam@gmail.com>
2019-04-02 14:25:39 +0200

    Wietse, test code voor het geluid met c++

--- test.cpp
+++ test.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    cout << "Play" << endl;
+    system("omxplayer -o local Darude\ -\ Sandstorm.mp3");
+    for (int i=0; i<1000; i++) {
+        cout << i << endl;
+    }
+    system("q");
+}
