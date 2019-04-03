24da0c36f0750e2e74540b19d42d03adfd0d2f0b
KevinPatist <kevinpatist@gmail.com>
2019-04-03 10:20:12 +0200

    functies om geluid af te spelen en te stoppen en geluiden in een map

---
+++ Main/sourceSound.cpp
@@ -0,0 +1,37 @@
+//
+// Created by kevin on 02/04/19.
+//
+#include "linefollower.h
+using namespace std;
+
+// playing the right sound by asking for a parameter
+void playSound(char & selection) {
+    switch(selection) {
+        case 'F':  //forward
+            system("omxplayer --no-keys -o local Noises/forward.wav &");
+            break;
+        case 'B':  //backwards
+            system("omxplayer --no-keys -o local Noises/backwards.wav &");
+            break;
+        case 'T':  //turn on spot
+            system("omxplayer --no-keys -o local Noises/turnonspot.wav &");
+            break;
+        case 'S':  //see object
+            system("omxplayer --no-keys -o local Noises/seeobject.wav &");
+            break;
+        case 'D':  //dodge object
+            system("omxplayer --no-keys -o local Noises/dodge.wav &");
+            break;
+        case 'C':  //crossroad
+            system("omxplayer --no-keys -o local Noises/crossroad.wav &");
+            break;
+        /*case 'X':  //plan X, playing megalovania while charging at the object
+            system("omxplayer --no-keys -o local Noises/megalovania.wav &");
+            break;*/
+    }
+}
+
+//stopping the sound because the time to play can be different
+void stopSound() {
+    system("killall omxplayer.bin")
+}
