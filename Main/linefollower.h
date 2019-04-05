f126952eb89aeed066cd86f375f78b97f32d750d
Stijn van Wijk <stijn.vanwijk@student.hu.nl>
2019-04-05 12:14:37 +0200

    STIJN:Test struct aggression

--- Main/linefollower.h
+++ Main/linefollower.h
@@ -35,6 +35,14 @@ struct CalculatingErrorData{
     int difference_max_avarage;
 };
 
+struct SharpCornerSettings{
+    float min_value_black = 0.36; //This value is the min. value needed by direction_control to be a sharp corner(black)
+    float min_value_white = 1.91;  //This value is the min. value needed by direction_control to be a sharp corner(white)
+    float max_aggression_black = 0.8;
+    float max_aggression_white = 0.6;
+};
+
+
 // sourceCrossroad.cpp
 void crossLine(BrickPi3 & BP, int32_t forward_degrees);
 void driveLeft(BrickPi3 & BP);
@@ -69,7 +77,7 @@ void defineDifferenceToAverage(CalculatingErrorData & data_struct);
 
 // sourcePIDController.cpp
 int defineError(int avarage_lowest_highest, int dist_lowest_avarage, int dist_highest_avarage, int current_measurment);
-void pController(int error_value, BrickPi3 & BP);
+void pController(int error_value, BrickPi3 & BP, SharpCornerSettings & StructSharp);
 
 // sourceSensors.cpp
 int getUltraSValue(uint8_t port, sensor_ultrasonic_t & data_struct, BrickPi3 & BP);
