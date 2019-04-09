626e4ff2e7215e30d9d4a2be3447cdd03e80d9d9
Wietse <WietseTDam@gmail.com>
2019-04-09 12:39:00 +0200

    rewirte of the lcd functions

--- Main/lcdtest.cpp
+++ Main/lcdtest.cpp
@@ -20,3 +20,75 @@ struct wiringPiValues {
     int fd;
 };
 
+
+
+int main() {
+    wiringPiValues val;
+    if (wiringPiSetup() == -1) exit (1);
+    val.fd = wiringPiI2CSetup(I2C_ADDR);
+    lcdStart();
+    while (true) {
+        resetLcd();
+        sleep(1);
+        cursorLocation(LINE1);
+        typeString("test");
+        cursorLocation(LINE2);
+        typeString("LOL");
+        sleep(1);
+    }
+}
+
+
+void clearLcd() {
+    lcd_byte(0x01, LCD_CMD);
+    lcd_byte(0x02, LCD_CMD);
+}
+
+void cursorLocation(int line) {
+    lcd_byte(line, LCD_CMD);
+}
+
+void typeString(const char *s) {
+    while (*s) lcd_byte(*(s++), LCD_CHR);
+}
+
+void lcd_byte(int bits, int mode)   {
+    wiringPiValues val;
+    //Send byte to data pins
+    // bits = the data
+    // mode = 1 for data, 0 for command
+    int bits_high;
+    int bits_low;
+    // uses the two half byte writes to LCD
+    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT ;
+    bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT ;
+
+    // High bits
+    wiringPiI2CReadReg8(val.fd, bits_high);
+    lcd_toggle_enable(bits_high);
+
+    // Low bits
+    wiringPiI2CReadReg8(val.fd, bits_low);
+    lcd_toggle_enable(bits_low);
+}
+
+void enableLcd(int bits) {
+    wiringPiValues val;
+    // Toggle enable pin on LCD display
+    delayMicroseconds(500);
+    wiringPiI2CReadReg8(val.fd, (bits | ENABLE));
+    delayMicroseconds(500);
+    wiringPiI2CReadReg8(val.fd, (bits & ~ENABLE));
+    delayMicroseconds(500);
+}
+
+void lcdStart() {
+    // Initialise display
+    lcd_byte(0x33, LCD_CMD); // Initialise
+    lcd_byte(0x32, LCD_CMD); // Initialise
+    lcd_byte(0x06, LCD_CMD); // Cursor move direction
+    lcd_byte(0x0C, LCD_CMD); // 0x0F On, Blink Off
+    lcd_byte(0x28, LCD_CMD); // Data length, number of lines, font size
+    lcd_byte(0x01, LCD_CMD); // Clear display
+    delayMicroseconds(500);
+}
