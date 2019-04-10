1a0dd052df2fa73fd3bb4402a7a9450f25a7d592
Nathan Houwaart <nathan.houwaart@student.hu.nl>
2019-04-10 12:30:31 +0200

    Added comments to the file

--- Main/sourcePController.cpp
+++ Main/sourcePController.cpp
@@ -3,27 +3,26 @@
 using namespace std;
 
 void PController(sensor_color_t & Color1, BrickPi3 & BP, CalculatingErrorData & data_struct) {
-    int tp = 360;
-    float kp = -1.00;
+    int tp = 360;                                           // Constant value to determine maximum motor dps
+    float kp = -3.50;                                       // Constant value to determine the sharpness of the turns the robot takes
     while (true) {
 
-        int offset = data_struct.avarage_min_max;
+        int offset = data_struct.avarage_min_max;           // Target ligh value for the robot to follow
 
-        BP.get_sensor(PORT_1, Color1);
-        int light_value = Color1.reflected_red;
-        if(light_value < data_struct.lowest_measurment){
+        BP.get_sensor(PORT_1, Color1);                      // Get sensor data
+        int light_value = Color1.reflected_red;             // Get sensor data
+        if(light_value < data_struct.lowest_measurment){    // Checks whether the lowest measured value needs to be updated
             data_struct.lowest_measurment = light_value;
             defineDifferenceToAverage(data_struct);
-        } else if(light_value > data_struct.highest_measurment){
+        } else if(light_value > data_struct.highest_measurment){ // Checks whether the hightest measured light value needs to be updated
             data_struct.highest_measurment = light_value;
-            defineDifferenceToAverage(data_struct);
+            defineDifferenceToAverage(data_struct);         // Recalculate data
         }
 
-        int error = light_value - offset;
-        int turn = kp * error;
-        int speedA = tp + turn;
-        int speedD = tp - turn;
-        MotorController(speedD, speedA, BP);
-        cout << speedA << " " << speedD << endl;
+        int error = light_value - offset;                   // Calculate error
+        int turn = kp * error;                              // Convert error value to turn value
+        int speedA = tp + turn;                             // Set new speed of motor A
+        int speedD = tp - turn;                             // Set new speed of motor B
+        MotorController(speedD, speedA, BP);                // Update motor speed
     }
 }
