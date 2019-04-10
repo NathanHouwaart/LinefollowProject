a18ef1af7f4ce26254fce5ed5804c9b3c5b48e38
Unknown <jochem-van-weelde@hotmail.nl>
2019-04-10 14:11:27 +0200

    added comments for turnUS and steeringRobot

--- Main/sourceDodgeObject.cpp
+++ Main/sourceDodgeObject.cpp
@@ -6,6 +6,9 @@ int current_position_us=0;
 vector<float> buffer_motor_values = {};
 
 float vectorAvarage(const vector<float> & to_calculate){
+	/*
+	 * This function calculates the average of the given value
+	 */
 	float sum = 0;
 	int total = 0;
 	for(float i : to_calculate){
@@ -16,43 +19,44 @@ float vectorAvarage(const vector<float> & to_calculate){
 }
 
 void turnUS (float values_wheels, BrickPi3 & BP){
+	/*
+	 * The motor turns the Ultrasonic sensor.
+	 * This function gets a value(float) between 0 en 2. (0: 90 degrees left, 1: 0 degrees(forward) 2: 90 degrees right)
+	 * The sensor turns with the road so it can detect an object while steering.
+	 * Then this function calculates the degrees to turn with the old calculations.
+	 * So it knows where the US sensor is looking at(degrees) and calculates the new value.
+	 * The difference between those values, is the turn.
+	 */
 	//values_wheels een waarde tussen 0 & 2
-	int max_degrees_turned = -95;
-	int min_degrees_turned = 95;
+	int max_degrees_turned = -95; //5 degrees buffer because the motor is not accurate all the time
+	int min_degrees_turned = 95; //5 degrees buffer because the motor is not accurate all the time
 	int target_degrees_us = 0;
 	int degrees_to_turn = 0;
 	uint8_t motor_middle = PORT_B; // Setting the motor to communicate
 	int buffersize = 0; //Moet 300 zijn
 	if(buffer_motor_values.size() < buffersize){
-		buffer_motor_values.push_back(values_wheels);
+		buffer_motor_values.push_back(values_wheels); //add one measurement to the  buffer(vector)
 	} else{
-		buffer_motor_values.push_back(values_wheels);
-		float avarage_of_vector = vectorAvarage(buffer_motor_values);
-		buffer_motor_values = {};
-		if(avarage_of_vector == 1){
+		buffer_motor_values.push_back(values_wheels); //If buffer is full (bigger then buffersize)
+		float avarage_of_vector = vectorAvarage(buffer_motor_values); //calculate average
+		buffer_motor_values = {}; //clean buffer for next object
+		if(avarage_of_vector == 1){ // look forward
 			target_degrees_us = 0;
-		} else if(avarage_of_vector < 1){
-			target_degrees_us = min_degrees_turned * (1-avarage_of_vector);
-		} else{
-			target_degrees_us = (max_degrees_turned * (avarage_of_vector-1));
+		} else if(avarage_of_vector < 1){ // look left
+			target_degrees_us = min_degrees_turned * (1-avarage_of_vector); //calculate how far the US needs to turn left
+		} else{ //look right
+			target_degrees_us = (max_degrees_turned * (avarage_of_vector-1)); //calculate how far the US needs to turn right
 		}
-
-		degrees_to_turn = target_degrees_us - current_position_us;
-
-//		cout << "avarage_of_vector: " << avarage_of_vector << endl;
-//		cout << "current_position " << current_position_us << endl;
-//		cout << "target_degrees_us " << target_degrees_us << endl;
-//		cout << "Degrees_to_turn " << degrees_to_turn  << endl;
-		current_position_us = target_degrees_us;
-		//BP.set_motor_limits(motor_middle, 100, 90); // Limit the motor so the gears can keep up
-		BP.set_motor_position_relative(motor_middle, degrees_to_turn);
+		degrees_to_turn = target_degrees_us - current_position_us; // Calculate the difference
+		current_position_us = target_degrees_us; // Set the new position for the next measurement
+		BP.set_motor_position_relative(motor_middle, degrees_to_turn); //Turn the motor
 	}
 }
 
 void steeringRobot(char to_steer, BrickPi3 & BP){
 	/*
 	 * This function is called when the robot is driving around the object
-	 *
+	 * The robot looks at the object en follows the circumference
 	 */
 	uint8_t motor_left = PORT_A;
 	uint8_t motor_right = PORT_D;
