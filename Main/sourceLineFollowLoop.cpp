49a8373fc713a63a4b9debb449ec0d7c01fb84ed
Unknown <jochem-van-weelde@hotmail.nl>
2019-04-11 15:26:41 +0200

    JOCHEM: Clean-up crew

--- Main/sourceLineFollowLoop.cpp
+++ Main/sourceLineFollowLoop.cpp
@@ -13,26 +13,32 @@ using namespace std;
 
 char choice_dodge_object;
 int counter_obstacle_detect = 0;
-int playing = 0;        //telling the program that no sound is currently playing
-int lcd_counter = 5000;    // to keep the lcd form updating every loop and than noging shows and start a 10000 to start the lcd
+int playing = 0;                        // telling the program that no sound is currently playing
+int lcd_counter = 5000;                 // to keep the lcd form updating every loop (updated every 5000 loops)
 
 void lineFollowLoop(sensor_color_t & Color1, sensor_color_t & Color2, sensor_ultrasonic_t & UltraSonic, CalculatingErrorData data_struct, int & fd, BrickPi3 & BP){
+    /*
+     * This function runs when the Linemode is selected at startup.
+     * This is the main linefollow function and keeps running forever.
+     * The robot will follow a line, drive around an object, play music, detect a crossroad and display something on the LCD
+     * CTRL + C will stop this loop.
+     */
     while (true) {
-        lcd_counter++;                  // add one to the counter for every loop
-        if (lcd_counter >= 5000) {      // after 5000 loops update hij het schermpje
+        lcd_counter++;                                          // add one to the counter for every loop
+        if (lcd_counter >= 5000) {                              // after 5000 loops the screen will be updated
             float battery = BP.get_voltage_battery();
             float battery_percentage = (100/(12.6-10.8)*(battery-10.8));
-            clearLcd(fd);   // clear the lcd
-            cursorLocation(LINE1, fd);      // set the cursorlocation to line 1
-            typeFloat(battery_percentage, fd);  // display the battery_percantage
-            cursorLocation(LINE2, fd);     // set the cursorlocation to line 2
-            typeString("PCT   Linefollow", fd);   // print the text on the screen
-            lcd_counter = 0;                    // reset the counter
+            clearLcd(fd);                                       // clear the lcd
+            cursorLocation(LINE1, fd);                          // set the cursorlocation to line 1
+            typeFloat(battery_percentage, fd);                  // display the battery_percentage
+            cursorLocation(LINE2, fd);                          // set the cursorlocation to line 2
+            typeString("PCT   Linefollow", fd);                 // print the text on the screen
+            lcd_counter = 0;                                    // reset the counter
         }
         BP.get_sensor(PORT_1, Color1);                          // Read colorsensor1 and put data in struct Color1
         BP.get_sensor(PORT_3, Color2);
         int main_sensor_measurment = Color1.reflected_red;
-        // Checks if current reading is bigger or smaller than the highest or lowest value, if true than calculate new average
+        // Checks if current reading is bigger or smaller than the highest or lowest value, if true then calculate new average
         if(main_sensor_measurment < data_struct.lowest_measurment){
             data_struct.lowest_measurment = main_sensor_measurment;
             defineDifferenceToAverage(data_struct);
@@ -41,37 +47,44 @@ void lineFollowLoop(sensor_color_t & Color1, sensor_color_t & Color2, sensor_ult
             defineDifferenceToAverage(data_struct);
         }
 
-        if (getUltraSValue(PORT_4, UltraSonic, BP) > 10) { // US bigger than 10 so check crossroad and line follow
+        if (getUltraSValue(PORT_4, UltraSonic, BP) > 10) {
+            // US bigger than 10 so check crossroad and line follow
             playSound('F', playing);
             counter_obstacle_detect = 0;
-            // Crossroad detected because both sensors lower than average
             if (Color2.reflected_red < data_struct.avarage_min_max && main_sensor_measurment < data_struct.avarage_min_max) {
+                // Crossroad detected because both sensors lower than average
                 playSound('C', playing);
                 crossroad(BP, playing, fd);
-                lcd_counter = 5000;       // to restart the lcd and give the battery percantage
-            } else {                                             // If no intersection was detected, follow the line
+                lcd_counter = 5000;                             // to restart the lcd and give the battery percantage
+            } else {
+                // If no intersection was detected, follow the line
                 int error_to_avarage = defineError(data_struct.avarage_min_max, data_struct.difference_min_avarage, data_struct.difference_max_avarage, main_sensor_measurment);
                 pController(error_to_avarage, BP);
             }
-        } else {                                    // If an object was detected within X cm, execute this code
+        } else {
+            // If an object was detected within X cm, execute this code
             playSound('S', playing);
-            drive(DIRECTION_STOP, 0, 360, BP);      // Stop the robot
+            drive(DIRECTION_STOP, 0, 360, BP);                  // Stop the robot
             counter_obstacle_detect++;
-            if (counter_obstacle_detect >= 500) {   // after 500 consecutive readings the robot enters this code
+            if (counter_obstacle_detect >= 500) {
+                // after 500 consecutive readings the robot enters this code
                 cout << "YEEBUG: I am in the obstacle detect." << endl;
-                counter_obstacle_detect = 0; // Makes sure we detect the next object correctly with the buffer
+                counter_obstacle_detect = 0;                    // Makes sure we detect the next object correctly with the buffer
                 playSound('D', playing);
-                bool correct_answer = false; // Makes it able to loop the question if the answer is incorrect
+                bool correct_answer = false;                    // Makes it able to loop the question if the answer is incorrect
 
                 while(!correct_answer){
+                    /*-----Ask for input until a correct char is given-----*/
                     cout << "Object detected: What do you want to do, type: D(dodge) or X(Plan X)" << endl;
                     cin >> choice_dodge_object;
                     switch (choice_dodge_object) {
-                        case 'X':           // Starts plan X, charging add the robot.
+                        case 'X':
+                            // Starts plan X, charging add the robot
                             megaCharge(playing, BP);
                             correct_answer = true;
                             break;
-                        case 'D':           // Starts dodging the object
+                        case 'D':
+                            // Starts dodging the object
                             driveAroundObject(UltraSonic, Color1, Color2, data_struct.avarage_min_max, BP);
                             correct_answer = true;
                             break;
--- Main/sourcePIDController.cpp
+++ Main/sourcePIDController.cpp
@@ -13,8 +13,9 @@ using namespace std;
 
 int defineError(int avarage_lowest_highest, int dist_lowest_avarage, int dist_highest_avarage, int current_measurment) {
     /* Function calculates how far off the current light measurement is off from the target value.
-     * Returns a positive percentage if sensor detects more white
-     * Returns a negative percentage if sensor detects more black */
+     * Returns a positive percentage if sensor detects more white.
+     * Returns a negative percentage if sensor detects more black.
+     */
 
     float steering_error, current_difference;
     if (current_measurment > avarage_lowest_highest) {
@@ -26,38 +27,33 @@ int defineError(int avarage_lowest_highest, int dist_lowest_avarage, int dist_hi
     } else {
         steering_error = 0;
     }
-//    cout << "steering_error: " << steering_error << endl;
-//    cout << "Act difference: " << current_difference << endl;
-//    cout << "higgest_difference: " << dist_highest_avarage << endl;
-//    cout << "laagste verschil: " << dist_lowest_avarage << endl;
     return steering_error;
 }
 
 void pController(int error_value, BrickPi3 & BP) {
-    /* In this function we calculate the speed and the direction for the light sensor*/
-    //TODO: --> wat is lijnwaarde precies? Is nu error_value, en dat is de error ten opzichte van de lijn tussen -100 en 100
-    //TODO: -->
-
+    /*In this function we calculate the speed and the direction for the light sensor*/
     int fine_tune_value = 0;
-    unsigned int power = 100;                       // The default value of the variable power is 100
-//	cout << error_value << endl;
-    float direction = (0.01 * error_value) + 1;     // Here we calcualte the direction between 0 and 2 for the function drive with the value you of the light sensor. With a P controller form the PID controller
-    if (direction > 1) {                            // If the direction is higher than 1 and the robot is going right we calcualte the speed, how shaper he turns how slower it goes
+    unsigned int power = 100;                           // The default value of the variable power is 100
+    float direction = (0.01 * error_value) + 1;         // Here we calculate the direction between 0 and 2 for the function drive with the value you of the light sensor. With a P controller form the PID controller
+    if (direction > 1) {
+        // If the direction is higher than 1 and the robot is going right we calculate the speed, how shaper he turns how slower it goes
         power = (1 - (direction - 1)) * 100;
-    } else if (direction < 1) {                       // Here does it the same for right but now it goes left
+    } else if (direction < 1) {
+        // Here does it the same for right but now it goes left
         power = (1 - direction) * 100;
-    } else {                                          // If the direction is 1 and the robot goes straight the power is 100
+    } else {
+        // If the direction is 1 and the robot goes straight the power is 100
         power = 100;
     }
     float limit = 50;
     // Here i reduce the scale of the power. Fist we have the range 0 to 100 and than you can change the range to 0 to limit to find the perfect speed
     power =fine_tune_value+(power-fine_tune_value)*(limit-fine_tune_value)/(100-fine_tune_value);
-    // If the power/ speed is lower than 10 the speed will be 10 to prevend the robot form stalling
+    // If the power/ speed is lower than 10 the speed will be 10 to prevent the robot form stalling
     if (power < 10){
         power = 10;
     } else if(power > 100){
         power = 100;
     }
     power = 100-power;
-    drive(direction,power,360,BP); // We give the direction and the speed to the function drive
+    drive(direction,power,360,BP);                      // We give the direction and the speed to the function drive
 }
